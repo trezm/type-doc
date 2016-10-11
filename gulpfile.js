b378aabd60678377e7aa985be1d4953cf1b2076f
George Fogle <fogle.george@gmail.com>
2016-10-10 20:39:45 -0400

    Add test page (#22)
    * install jshint as dev dep
    
    * basic node server to serve up the docs
    
    * add file input and watch for the file upload
    
    * add POST and validation route for files
    
    * add textarea for user entered code to validate
    
    * use errors coming back from typechecker

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp');
 const plugins = require('gulp-load-plugins')();
+const clean = plugins.clean;
+const sequence = require('run-sequence');
 
 gulp.task('build:dev', () => {
   return gulp.src('src/**/*.js')
@@ -21,6 +23,29 @@ gulp.task('test', ['build:dev'], () => {
     .once('end', () => {
       process.exit();
     });
+});
+
+/**
+ * DOCS
+ *
+ * - Clean existing docs/dist folder to get most recent type checker
+ * - Rebuild the existing dist folder
+ * - Copy over the rebuilt dist folder
+ */
+gulp.task('clean:docs', () => {
+  return gulp.src('docs/dist', { read: false }).pipe(clean());
+});
+
+gulp.task('copy:dist2docs', () => {
+  return gulp.src(['dist/**/*']).pipe(gulp.dest('docs/dist'));
+});
+
+gulp.task('docs', () => {
+  sequence(
+    'default',
+    'clean:docs',
+    'copy:dist2docs'
+  );
 });
 
 gulp.task('default', ['build:dev']);
